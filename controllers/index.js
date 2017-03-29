fa1c66038b593f1cfbda225f43e3b77a62e940bf
stf1094 <s.forsberg@alvariumsystems.com>
2017-03-29 15:15:35 -0400

    New
    New

---
+++ controllers/index.js
@@ -0,0 +1,51 @@
+/**
+ * Global Navigation Handler
+ */
+Alloy.Globals.Navigator = {
+
+	/**
+	 * Handle to the Navigation Controller
+	 */
+	navGroup : $.nav,
+
+	open : function(controller, payload) {
+		var win = Alloy.createController(controller, payload || {}).getView();
+
+		if (OS_IOS) {
+			$.nav.openWindow(win);
+		} else if (OS_MOBILEWEB) {
+			$.nav.open(win);
+		} else {
+
+			// added this property to the payload to know if the window is a child
+			if (payload.displayHomeAsUp) {
+
+				win.addEventListener('open', function(evt) {
+					var activity = win.activity;
+					activity.actionBar.displayHomeAsUp = payload.displayHomeAsUp;
+					activity.actionBar.onHomeIconItemSelected = function() {
+						evt.source.close();
+					};
+				});
+			}
+			win.open();
+		}
+	}
+};
+
+/** Open appropriate start window **/
+
+
+
+
+
+	if (OS_IOS) {
+		$.nav.open();
+	} else if (OS_MOBILEWEB) {
+		$.index.open();
+	} else {
+		$.index.getView().open();
+	}
+
+
+
